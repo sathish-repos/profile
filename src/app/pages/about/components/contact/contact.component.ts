e20c3d8e971e651f3b96b914fdeb6971a16e5073
sathish <me.sathish.git@gmail.com>
2024-05-18 23:14:05 +0530

    copy to clipboard add

--- src/app/pages/about/components/contact/contact.component.ts
+++ src/app/pages/about/components/contact/contact.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { CdkCopyToClipboard } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'skp-contact',
   standalone: true,
-  imports: [],
+  imports: [CdkCopyToClipboard],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
+  email: string = 'me.sathish.git@gmail.com';
 
+  async onCopy() {
+    await setInterval(() => {
+      this.email = 'me.sathish.git@gmail.com';
+    }, 800);
+    this.email = 'Email Copied !!';
+  }
 }
