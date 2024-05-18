9df378c2900a805b8227e27252215962b28f0fde
sathish <me.sathish.git@gmail.com>
2024-05-18 15:04:16 +0530

    setup routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'about',
+    loadComponent: () => import('./pages/about/about.component'),
+  },
+  { path: '', component: ProfileComponent },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./pages/page-not-found/page-not-found.component'),
+  },
+];
