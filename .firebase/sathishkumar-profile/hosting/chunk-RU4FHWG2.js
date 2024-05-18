edfcbcf0205fb0f1362ebd27282c7f3883acbfac
sathishkumar <160379406+sathish-repos@users.noreply.github.com>
2024-05-18 18:55:54 +0530

    Delete .firebase directory

--- .firebase/sathishkumar-profile/hosting/chunk-RU4FHWG2.js
+++
@@ -1,1 +0,0 @@
-import{Aa as s,R as o,ua as a,va as i,ya as r}from"./chunk-NZTZWSJS.js";var f=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["skp-about"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,c){n&1&&(a(0,"p"),r(1,"about works!"),i())}});let t=e;return t})();export{f as default};
