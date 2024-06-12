fbc8bfa6f32e9dfd144d9a64cc0d1bb9bb9e0c6a
sathish <me.sathish.git@gmail.com>
2024-06-12 19:46:55 +0530

    made self closing tag & apps comp

---
+++ src/app/pages/profile/components/apps/apps.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'skp-apps',
+  standalone: true,
+  imports: [],
+  templateUrl: './apps.component.html',
+  styleUrl: './apps.component.scss'
+})
+export class AppsComponent {
+
+}
