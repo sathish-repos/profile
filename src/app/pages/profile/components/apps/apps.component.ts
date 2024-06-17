a15695adc15b070f02688568f270ed434efadd31
sathish <me.sathish.git@gmail.com>
2024-06-17 08:33:44 +0530

    apps section change

--- src/app/pages/profile/components/apps/apps.component.ts
+++ src/app/pages/profile/components/apps/apps.component.ts
@@ -5,8 +5,60 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './apps.component.html',
-  styleUrl: './apps.component.scss'
+  styleUrl: './apps.component.scss',
 })
 export class AppsComponent {
-
+  content = {
+    title: 'Projects',
+    appsList: [
+      {
+        name: 'Todo',
+        description: 'Its an todo application with firebase as a backend',
+        imageUrl: {
+          mobile: 'assets/app-2.webp',
+          desktop: 'assets/todo.png',
+        },
+        link: {
+          url: 'https://todo-firebase-123.web.app/',
+          target: '_blank',
+        },
+      },
+      {
+        name: 'Todo',
+        description: 'Its an todo application with firebase as a backend',
+        imageUrl: {
+          mobile: 'assets/app-2.webp',
+          desktop: 'assets/todo.png',
+        },
+        link: {
+          url: 'https://todo-firebase-123.web.app/',
+          target: '_blank',
+        },
+      },
+      {
+        name: 'Todo',
+        description: 'Its an todo application with firebase as a backend',
+        imageUrl: {
+          mobile: 'assets/app-2.webp',
+          desktop: 'assets/todo.png',
+        },
+        link: {
+          url: 'https://todo-firebase-123.web.app/',
+          target: '_blank',
+        },
+      },
+      {
+        name: 'Todo',
+        description: 'Its an todo application with firebase as a backend',
+        imageUrl: {
+          mobile: 'assets/app-2.webp',
+          desktop: 'assets/todo.png',
+        },
+        link: {
+          url: 'https://todo-firebase-123.web.app/',
+          target: '_blank',
+        },
+      },
+    ],
+  };
 }
